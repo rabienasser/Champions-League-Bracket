79b91cb2741e090437d4911561deb5d4708124e0
Rabie <robienasser13@gmail.com>
2021-05-11 19:39:46 -0400

    team-row-selection

---
+++ app.js
@@ -0,0 +1,26 @@
+// // const circle = document.querySelectorAll('.circle')
+// const teamRow = document.querySelectorAll('.team-row')
+// const circles = document.querySelectorAll('.circle')
+
+// teamRow.forEach(row => {
+//     // const id = Math.floor(Math.random() * 100)
+//     row.addEventListener('click', () => {
+//         row.classList.add('selected-row')
+//             blah();
+//         // if(row.classList.contains('selected-row')) {
+//         //     const circle = document.querySelectorAll('.circle')
+//         //     circle.forEach(circle => {
+//         //         circle.classList.add('checked-circle')
+//         //         circle.innerHTML = '<i class="fas fa-check"></i>'
+//         //     })
+//         // }
+//     })
+// })
+
+// function blah() {
+//     circles.forEach(circle => {
+//             circle.classList.add('checked-circle')
+//             circle.innerHTML = 'X'
+//     })
+// }
+
