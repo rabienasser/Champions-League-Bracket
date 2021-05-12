774c1b4c92a21edf9089f48497582c8f4bc364b7
Rabie <robienasser13@gmail.com>
2021-05-12 16:59:28 -0400

    team-selection

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
-// // const circle = document.querySelectorAll('.circle')
+const popUp = document.querySelector('.pop-up-container')
+const popUpCloseBtn = document.querySelector('.pop-up-close')
+const viewDetails = document.querySelectorAll('.view-details')
+
+const circle = document.querySelectorAll('.circle')
 // const teamRow = document.querySelectorAll('.team-row')
 // const circles = document.querySelectorAll('.circle')
 
@@ -24,3 +28,37 @@
 //     })
 // }
 
+const q1 = document.querySelectorAll('.q-1')
+const s1 = document.querySelector('.s-1')
+const s2 = document.querySelector('.s-2')
+const team1 = document.querySelector('.team-1')
+const team2 = document.querySelector('.team-2')
+
+q1.forEach(team => {
+    team.addEventListener('click', () => {
+        s1.innerHTML = team.innerHTML;
+        s1.classList.remove('empty-row')
+     })
+})
+
+function selectRow() {
+    team1.addEventListener('click', () => {
+        team1.classList.add('selected-row')
+        team2.classList.remove('selected-row')
+    })
+    team2.addEventListener('click', () => {
+        team2.classList.add('selected-row')
+        team1.classList.remove('selected-row')
+    })
+}
+
+selectRow();
+
+viewDetails.forEach(match => {
+    match.addEventListener('click', () => {
+        popUp.style.display = 'flex';
+        popUpCloseBtn.addEventListener('click', () => {
+            popUp.style.display = 'none'
+        })
+    })
+})
