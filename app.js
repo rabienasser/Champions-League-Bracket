05c0635b9b3ea6cce087639f818da3c2f8147be6
Rabie <robienasser13@gmail.com>
2021-05-18 17:12:19 -0400

    data-attribute-obtained

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const popUp = document.querySelector('.pop-up-container')
-const viewDetails = document.querySelectorAll('.view-details')
+// const viewDetails = document.querySelectorAll('.view-details')
 const popUpContainer = document.querySelector('.pop-up-container')
 const bracketContent = document.querySelector('.bracket-content')
 const s1 = document.querySelector('.s-1')
@@ -42,7 +42,7 @@ function addFinalTeams(semi, final, finalArray) {
         const target = e.target
     
         if(target !== e.currentTarget) {
-            if(target.classList.contains(semi)) {
+            if(target.classList.contains(semi) && !target.classList.contains('empty-row')) {
                 finalArray.push(target)
                 final.innerHTML = target.innerHTML;
                 final.classList.remove('empty-row')
@@ -122,81 +122,103 @@ function test() {
 
 
 // Create view details
-viewDetails.forEach(match => {
-    match.addEventListener('click', createPopUp)
-})
-    
+// viewDetailsArray = Array.from(viewDetails)
+
+// viewDetailsArray.forEach(match => {
+//     match.addEventListener('click', createPopUp)
+// })
+
+
 
 
 // CREATE POPUP SCREEN OF MATCH PREVIEW
 function createPopUp() {
-    fetch("https://api-football-v1.p.rapidapi.com/v3/teams?name=FC%20Porto", {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-key": "576022dfeamsh7ad87ea6befc8bep1031c3jsnf1d4181025ac",
-		"x-rapidapi-host": "api-football-v1.p.rapidapi.com"
-	}
-})
-    .then(response => response.json())
-    .then(data => {
-        console.log(data)
-        const team = data.response[0].team;
 
-        const popUp = document.createElement('div')
-        popUp.classList.add('pop-up-overlay')
-        popUp.innerHTML =  `
-        <div class="pop-up">
-        <button class="pop-up-close">X</button>
-        <h3>You picked<br>
-        <span class="selected-team">Bayern</span> to beat Paris</h3>
-        <div class="match">
-            <img src="./images/bayern munich.png" class="team-logo fixture-logo selected-team-logo">
-            <p>VS</p>
-            <img src="./images/PSG.png" class="team-logo fixture-logo">
-        </div>
-        <div class="recent-results">
-            <p>Recent fixtures</p>
-            <div class="fixture">
-                <div class="date">WED 7 APR</div>
-                <div class="recent-game">
-                    <div class="team">
-                        <img src="${team.logo}" class="team-logo">
-                        <p class="team-name">${team.name}</p> 
-                    </div>
-                    <p>3 - 2</p>
-                    <div class="team">
-                        <img src="./images/psg.png" class="team-logo">
-                        <p class="team-name">PSG</p> 
-                    </div>
-                    <i class="fas fa-angle-right"></i>
-                </div>
-            </div>
-            <div class="fixture">
-                <div class="date">TUES 13 APR</div>
-                <div class="recent-game">
-                    <div class="team">
-                        <img src="./images/bayern munich.png" class="team-logo">
-                        <p class="team-name">Bayern</p> 
-                    </div>
-                    <p>2 - 3</p>
-                    <div class="team">
-                        <img src="./images/psg.png" class="team-logo">
-                        <p class="team-name">PSG</p> 
-                    </div>
-                    <i class="fas fa-angle-right"></i>
-                </div>
-            </div>
-        </div>
-        </div>
-        `;
-        popUpContainer.appendChild(popUp)
-    })
-    .catch(err => {
-        console.error(err);
-    });
+    bracketContent.addEventListener('click', (e) => {
+        const target = e.target
     
-    }
+        if(target !== e.currentTarget) {
+            if(target.classList.contains('view-details')) {
 
+                const firstTeam = target.previousElementSibling.firstElementChild.firstElementChild.nextElementSibling;
+                const teamID_1 = firstTeam.getAttribute('data-teamID')
+                
+                const secondTeam = target.previousElementSibling.previousElementSibling.firstElementChild.firstElementChild.nextElementSibling;
+                const teamID_2 = secondTeam.getAttribute('data-teamID')
+
+
+                fetch(`https://api-football-v1.p.rapidapi.com/v3/fixtures/headtohead?h2h=${teamID_1}-${teamID_2}`, {
+                    "method": "GET",
+                    "headers": {
+                        "x-rapidapi-key": "576022dfeamsh7ad87ea6befc8bep1031c3jsnf1d4181025ac",
+                        "x-rapidapi-host": "api-football-v1.p.rapidapi.com"
+                    }
+                })
+                    .then(response => response.json())
+                    .then(data => {
+                        console.log(data)
+                        const team_1 = data.response[0].teams.away;
+                        const team_2 = data.response[0].teams.home;
+                
+                        const popUp = document.createElement('div')
+                        popUp.classList.add('pop-up-overlay')
+                        popUp.innerHTML =  `
+                        <div class="pop-up">
+                        <button class="pop-up-close">X</button>
+                        <h3>You picked<br>
+                        <span class="selected-team">${team_1.name}</span> to beat ${team_2.name}</h3>
+                        <div class="match">
+                            <img src="${team_1.logo}" class="team-logo fixture-logo selected-team-logo">
+                            <p>VS</p>
+                            <img src="${team_2.logo}" class="team-logo fixture-logo">
+                        </div>
+                        <div class="recent-results">
+                            <p>Recent fixtures</p>
+                            <div class="fixture">
+                                <div class="date">WED 7 APR</div>
+                                <div class="recent-game">
+                                    <div class="team">
+                                        <img src="${team_1.logo}" class="team-logo">
+                                        <p class="team-name">${team_1.name}</p> 
+                                    </div>
+                                    <p>3 - 2</p>
+                                    <div class="team">
+                                        <img src="./images/psg.png" class="team-logo">
+                                        <p class="team-name">PSG</p> 
+                                    </div>
+                                    <i class="fas fa-angle-right"></i>
+                                </div>
+                            </div>
+                            <div class="fixture">
+                                <div class="date">TUES 13 APR</div>
+                                <div class="recent-game">
+                                    <div class="team">
+                                        <img src="./images/bayern munich.png" class="team-logo">
+                                        <p class="team-name">Bayern</p> 
+                                    </div>
+                                    <p>2 - 3</p>
+                                    <div class="team">
+                                        <img src="" class="team-logo">
+                                        <p class="team-name">PSG</p> 
+                                    </div>
+                                    <i class="fas fa-angle-right"></i>
+                                </div>
+                            </div>
+                        </div>
+                        </div>
+                        `;
+                        popUpContainer.appendChild(popUp)
+                    })
+                    .catch(err => {
+                        console.error(err);
+                    });
+            }
+        }
+    
+        e.stopPropagation();
+    });
+    }
+    createPopUp();
 
     // POP-UP CLOSE BUTTON USING EVENT DELEGATION
     popUp.addEventListener('click', (e) => {
