4b02d5e9d85c0a1b28abf3a90de023b5f62d4cde
Rabie <robienasser13@gmail.com>
2021-05-25 18:01:36 -0400

    create-email-regex

--- login.js
+++ login.js
@@ -1,3 +1,10 @@
+const createAcctOverlay = document.querySelector('.create-account-overlay')
+const createAcctBtn = document.querySelector('.create-account-btn')
+const closeBtn = document.querySelector('.close-create-acct')
+const alreadyHaveAcctBtn = document.querySelector('.already-have-acct-btn')
+const createAcctForm = document.querySelector('.create-acct-form')
+
+
 // GENERATE UEFA LOG-IN SCREEN
 function UEFALogIn() {
     const loginOverlay = document.querySelector('.log-in-overlay')
@@ -5,6 +12,8 @@ function UEFALogIn() {
 
     loginOverlay.style.opacity = 1
     loginOverlay.style.zIndex = 3
+    createAcctOverlay.style.opacity = 0
+    createAcctOverlay.style.zIndex = 1
 
     closeBtn.addEventListener('click', () => {
         loginOverlay.style.opacity = 0
@@ -17,46 +26,7 @@ function UEFALogIn() {
 
 
 
-// GENERATE UEFA CREATE-ACCOUNT SCREEN
-function createAcct(loginOverlay) {
-    const createAcctOverlay = document.querySelector('.create-account-overlay')
-    const createAcctBtn = document.querySelector('.create-account-btn')
-    const closeBtn = document.querySelector('.close-create-acct')
-
-    createAcctBtn.addEventListener('click', () => {
-        loginOverlay.style.opacity = 0
-        loginOverlay.style.zIndex = 1
-        createAcctOverlay.style.opacity = 1
-        createAcctOverlay.style.zIndex = 3
-    })
-
-    closeBtn.addEventListener('click', () => {
-        createAcctOverlay.style.opacity = 0
-        createAcctOverlay.style.zIndex = 1
-    })
-
-    inputHover();
-}
-
-
-
-// HOVER OVER QUESTION ON INPUTS ON CREATE-ACCOUNT SCREEN
-function inputHover() {
-    const icon1 = document.querySelector('.q-hover-1')
-    const icon2 = document.querySelector('.q-hover-2')
-    const icon3 = document.querySelector('.q-hover-3')
-    const msg1 = document.querySelector('.hover-msg-1')
-    const msg2 = document.querySelector('.hover-msg-2')
-    const msg3 = document.querySelector('.hover-msg-3')
-
-    icon1.addEventListener('mouseover', () => {
-        msg1.style.opacity = 1;
-    })
-}
-
-
-
-// SUBMIT EMAIL AND PASSWORD ON LOGIN
+// SUBMIT EMAIL AND PASSWORD ON LOGIN SCREEN
 function submitLogIn() {
     const submitBtn = document.querySelector('.submit-btn')
     const invalidMsg = document.querySelector('.invalid-msg')
@@ -66,7 +36,7 @@ function submitLogIn() {
     const password = document.querySelector('.password')
 
     // LOAD & PREVENT DEFAULT ON SUBMIT BUTTON
-    submitBtn.addEventListener('click', (e) => {
+    form.addEventListener('submit', (e) => {
         loader.style.display = 'block'
         invalidMsg.style.display = 'none'
 
@@ -80,43 +50,65 @@ function submitLogIn() {
 
         e.preventDefault()
     })
+}
 
-    // LOAD & PREVENT DEFAULT ON ENTER KEY
-    form.addEventListener('keypress', (e) => {
-        if(e.keyCode == 13 || e.which == 13) {
 
-            loader.style.display = 'block'
-            invalidMsg.style.display = 'none'
-    
-            setTimeout(function() {
-                invalidMsg.style.display = 'flex'
-                loader.style.display = 'none'
-            }, 1000)
-            
-            email.value = ''
-            password.value = ''
 
-            e.preventDefault()
-        }
+// GENERATE UEFA CREATE-ACCOUNT SCREEN
+function createAcct(loginOverlay) {
+    createAcctBtn.addEventListener('click', () => {
+        loginOverlay.style.opacity = 0
+        loginOverlay.style.zIndex = 1
+        createAcctOverlay.style.opacity = 1
+        createAcctOverlay.style.zIndex = 3
+    })
 
+    closeBtn.addEventListener('click', () => {
+        createAcctOverlay.style.opacity = 0
+        createAcctOverlay.style.zIndex = 1
+    })
+
+    iconHover();
+}
+
+
+
+// HOVER OVER QUESTION ON INPUTS ON CREATE-ACCOUNT SCREEN
+function iconHover() {
+    const msg1 = document.querySelector('.hover-msg-1')
+    const msg2 = document.querySelector('.hover-msg-2')
+    const msg3 = document.querySelector('.hover-msg-3')
+    const icon = document.querySelectorAll('.fa-question-circle')
+
+    icon.forEach(icon => {
+        icon.addEventListener('mouseover', () => {
+            if(icon.classList.contains('q-hover-1')) msg1.style.opacity = 1
+            else if(icon.classList.contains('q-hover-2')) msg2.style.opacity = 1
+            else if(icon.classList.contains('q-hover-3')) msg3.style.opacity = 1
+        })
+        icon.addEventListener('mouseout', () => {
+            if(icon.classList.contains('q-hover-1')) msg1.style.opacity = 0
+            else if(icon.classList.contains('q-hover-2')) {msg2.style.opacity = 0}
+            else if(icon.classList.contains('q-hover-3')) msg3.style.opacity = 0
+        })
     })
 }
 
 
 
-// // CREATE EMAIL
-// function createEmail() {
-//     re = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/
-//     const error = document.querySelector('.email-error')
+// CREATE EMAIL
+function createEmail() {
+    re = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/
+    const email = document.querySelector('.email2')
+    const error = document.querySelector('.email-error')
 
-//     if(!re.test(email.value)) {
-//         email.classList.add('invalid')
-//         error.style.display = 'flex'
-//     } else {
-//         email.classList.add('valid')
-//         error.style.display = 'none'
-//     }
-// }
+    if(!re.test(email.value)) {
+        error.style.display = 'flex'
+    } else {
+        console.log('success')
+        error.style.display = 'none'
+    }
+}
 
 
 
@@ -146,5 +138,14 @@ function submitLogIn() {
 // }
 
 
+
+// EVENT LISTENERS
 // PULL LOGIN SCREEN WHEN LOGIN BTN IS PRESSED
 loginBtn.addEventListener('click', UEFALogIn)
+
+alreadyHaveAcctBtn.addEventListener('click', UEFALogIn)
+
+createAcctForm.addEventListener('submit', (e) => {
+    createEmail();
+    e.preventDefault();
+})
